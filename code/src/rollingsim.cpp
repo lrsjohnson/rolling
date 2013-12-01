df6db56d0d381f0b7f882e8df2f77772245ffeaf
Lars Johnson <larsj@mit.edu>
2013-11-30 22:51:00 -0600

    Added boost rk solver

--- code/src/rollingsim.cpp
+++ code/src/rollingsim.cpp
@@ -1,19 +1,23 @@
 #include "rollingsim.h"
-
 #include "Vector3f.h"
 
-#include <iostream>
+#include <iostream> 
+#include <vector> 
 
 using std::cout;
 using std::endl;
+using std::vector;
 
 RollingSimulation::RollingSimulation() {
   world_ = new RollingWorld();
   ball_ = new RollingBall(Vector3f(0,0,0), 1);
+  stepper_ = new runge_kutta4<system_state_t>();
 };
 
 RollingSimulation::~RollingSimulation() {
   delete world_;
+  delete ball_;
+  delete stepper_;
 };
 
 void RollingSimulation::draw() {
@@ -21,6 +25,26 @@ void RollingSimulation::draw() {
   ball_->draw();
 };
 
+RollingSimulation::system_state_t RollingSimulation::getState() {
+  vector<double> state;
+  state.push_back(ball_->rotation);
+  state.push_back(3.0);
+  return state;
+};
+
+void RollingSimulation::setState(system_state_t state) {
+  ball_->rotation = state[0];
+};
+
 void RollingSimulation::step(float time_step) {
-  ball_->rotation += 360 * time_step / 3.0;
+  system_state_t out(2);
+  stepper_->do_step(system_func, getState(), 0, out, time_step);
+  setState(out);
+};
+
+void RollingSimulation::system_func(const system_state_t& x,
+				    system_state_t& dxdt,
+				    const double t) {
+  dxdt[0] = x[1];
+  dxdt[1] = -1000; // Force on rotation
 };
--- code/src/rollingsim.h
+++ code/src/rollingsim.h
@@ -4,17 +4,34 @@
 #include "rollingworld.h"
 #include "rollingball.h"
 
+#include <boost/numeric/odeint.hpp> 
+#include <vector> 
+
+using std::vector;
+using boost::numeric::odeint::runge_kutta4;
+
 class RollingSimulation {
  public:
   RollingSimulation();
   ~RollingSimulation();
-
+  
+  typedef vector<double> system_state_t;
+  
   void draw();
   void step(float time_step);
+
+  system_state_t getState();
+  void setState(system_state_t);  
+  
+  // Call for the boost runge-kutta stepper
+  static void system_func(const system_state_t& x,
+			  system_state_t& dxdt,
+			  const double t);
   
  private:
   RollingWorld* world_;
   RollingBall* ball_;
+  runge_kutta4<system_state_t>* stepper_;
 };
 
 #endif // ifndef ROLLING_SIM_H
